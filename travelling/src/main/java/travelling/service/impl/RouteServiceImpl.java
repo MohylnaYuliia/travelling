d17dffcea7aa8b15067a31ca56668f51b3ba7ef3
Yuliia Mohylna <mohylna.yuliia@gmail.com>
2022-05-17 02:42:33 +0300

    added implementation for getting all routes

--- travelling/src/main/java/travelling/service/impl/RouteServiceImpl.java
+++ travelling/src/main/java/travelling/service/impl/RouteServiceImpl.java
@@ -1,16 +1,25 @@
 package travelling.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import travelling.entity.RouteEntity;
+import travelling.entity.UserEntity;
+import travelling.repository.RouteRepository;
 import travelling.service.RouteService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RouteServiceImpl implements RouteService {
 
+    @Autowired
+    private RouteRepository routeRepository;
+
     @Override
     public List<RouteEntity> getAllRouts() {
-        return null;
+        List<RouteEntity> result = new ArrayList<>();
+        routeRepository.findAll().forEach(result::add);
+        return result;
     }
 }
