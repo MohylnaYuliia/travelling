95b25ad844afcbdc6551ef22115e8c98d8090875
Yuliia Mohylna <mohylna.yuliia@gmail.com>
2022-05-20 15:14:36 +0300

    added implementation for test to controller for flexible cancellation

--- travelling/src/main/java/travelling/controller/RouteController.java
+++ travelling/src/main/java/travelling/controller/RouteController.java
@@ -31,6 +31,6 @@ public class RouteController {
 
     @DeleteMapping("/{routeId}/users/{userId}/spots/{spotsNumber}")
     public void cancelSpots(@PathVariable Integer routeId, @PathVariable Integer userId, @PathVariable Integer spotsNumber) {
-
+        routeService.cancelReservation(userId, routeId, spotsNumber);
     }
 }
