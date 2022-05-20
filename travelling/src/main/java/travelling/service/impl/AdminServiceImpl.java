4e850401f6f601c16965b548a0ee6c6034921f9e
Yuliia Mohylna <mohylna.yuliia@gmail.com>
2022-05-20 16:18:54 +0300

    added implementation for test to admin service

--- travelling/src/main/java/travelling/service/impl/AdminServiceImpl.java
+++ travelling/src/main/java/travelling/service/impl/AdminServiceImpl.java
@@ -1,15 +1,24 @@
 package travelling.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import travelling.entity.UserRouteEntity;
+import travelling.repository.UserRouteRepository;
 import travelling.service.AdminService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private UserRouteRepository userRouteRepository;
+
     @Override
     public List<UserRouteEntity> getAllInformation() {
-        return null;
+        List<UserRouteEntity> result = new ArrayList<>();
+        userRouteRepository.findAll().forEach(result::add);
+        return result;
     }
 }
